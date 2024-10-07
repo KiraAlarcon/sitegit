6f1e8760cff9a1d26539a1ed4812a8f31b47bc68
KiraAlarcon <127362040+KiraAlarcon@users.noreply.github.com>
2024-10-07 19:16:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('form-contato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado pelo seu contato! Nós responderemos em breve.');
+});
